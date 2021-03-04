78b9c80ca748bd4356ffab29e33c56711291233f
Sudheer Kokkirigadda <sudheer.kokkirigadda1@t-mobile.com>
2021-03-04 10:07:52 +0530

    initial commit

---
+++ apps/books/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'books-demo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'books';
+}
